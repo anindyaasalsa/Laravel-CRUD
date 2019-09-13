4c254673dff385dea178e072cc8968f962cad8e8
root <root@debian.winxskye.com>
2019-09-13 00:31:32 -0700

    commit pertama sendiri

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+
+//tambahkan ini
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
+
+
+Route::middleware(['jwt.verify'])->group(function(){
+	// Route::get('book', 'BookController@book');
+	// Route::get('bookall', 'BookController@bookAuth');
+	// Route::get('user', 'UserController@getAuthenticatedUser');
+	Route::get('/book', 'BookController@index');
+	Route::get('/book/{id}', 'BookController@show');
+	Route::post('/book', 'BookController@store');
+	Route::put('/book/{id}', 'BookController@update');
+	Route::delete('/book/{id}', 'BookController@destroy');
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
